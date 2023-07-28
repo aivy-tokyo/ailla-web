2f01be2d5dfd78dcf0bdf83c6bbe3f295b7ca03a
Nobuhiko Futagami <nobuhiko.futagami@cureapp.jp>
2023-07-28 15:00:12 +0900

    終了ボタンのスタイル修正

--- components/EndTalkButton.tsx
+++ components/EndTalkButton.tsx
@@ -1,8 +1,33 @@
 //シチュエーション会話終了ボタン
 const endTalkButton = () => {
+  /* 
+  button style
+  background-color: transparent;
+    color: white;
+    border: 1px solid;
+    padding: 2px;
+    line-height: 1;
+    height: 28px;
+    width: 7em;
+    margin-top: 6px;
+    font-size: 12px;
+}
+  */
   return (
-    <button className="bg-red-400 w-28 text-black font-bold rounded-md flex justify-center items-center mr-2">
-      <span>終了</span>
+    <button className="
+    bg-transparent
+    text-white
+    border
+    border-white
+    p-1
+    h-7
+    w-28
+    mt-1.5
+    mr-1.5
+    text-sm
+    rounded-md
+    ">
+      <span>会話を終了</span>
     </button>
   );
 };
