c20418886a332724e0aa3ee15b64808fd74c3324
n-futagami <n-futagami@ubweb.works>
2023-08-23 23:15:48 +0900

    ボタンカラー調整

--- components/TranslateToggleSwitch.tsx
+++ components/TranslateToggleSwitch.tsx
@@ -8,7 +8,7 @@ const TranslateToggleSwitch = () => {
   };
 
   return (
-    <div className={`relative w-[64px] h-[31px] border cursor-pointer ${isTranslated ? 'border-sky-500 bg-stone-300 text-black' : 'border-slate-500 bg-slate-800 text-stone-300'} rounded-full flex items-center px-[1px]`} onClick={() => handleTranslate()}>
+    <div className={`relative w-[64px] h-[31px] border cursor-pointer ${isTranslated ? 'border-sky-500 bg-sky-400' : 'border-slate-500 bg-slate-800'} text-stone-300 rounded-full flex items-center px-[1px]`} onClick={() => handleTranslate()}>
       <div className={`w-[26px] h-[26px] rounded-full self-center transform transition-transform  ease-in-out duration-200 ${isTranslated ? 'translate-x-[33px] bg-sky-500' : 'translate-x-0 bg-slate-700' }`}></div>
       {isTranslated ?
         <span className="absolute left-2 text-xs">翻訳</span>
