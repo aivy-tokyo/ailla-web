9972fbcb4662aa86ea4a3fc63442c6ea218a93d5
n-futagami <n-futagami@ubweb.works>
2023-09-03 02:22:04 +0900

    フォーム修正

--- components/SettingContainer.tsx
+++ components/SettingContainer.tsx
@@ -1,21 +1,36 @@
-import React, { ChangeEvent } from 'react';
-import { signOut } from 'next-auth/react';
-import { useSetAtom } from 'jotai';
-import { FaRegTimesCircle } from 'react-icons/fa';
-import { avatarPathAtom, backgroundImagePathAtom, textToSpeechApiTypeAtom } from '../utils/atoms';
-import { avatars, backgroundImages, textToSpeechApiTypeList } from '../utils/constants';
-import { SelectedLanguageType, TextToSpeechApiType } from '../utils/types';
-import UserInfo from './UserInfo';
+import React, { ChangeEvent } from "react";
+import { signOut } from "next-auth/react";
+import { useAtom, useSetAtom } from "jotai";
+import { MdClose } from "react-icons/md";
+import {
+  avatarPathAtom,
+  backgroundImagePathAtom,
+  textToSpeechApiTypeAtom,
+} from "../utils/atoms";
+import {
+  avatars,
+  backgroundImages,
+  textToSpeechApiTypeList,
+} from "../utils/constants";
+import { TextToSpeechApiType } from "../utils/types";
+import UserInfo from "./UserInfo";
+import { HeaderLabel } from "./HeaderLabel";
 
 interface SettingContainerProps {
   onClose: () => void;
 }
 
-export const SettingContainer: React.FC<SettingContainerProps> = ({ onClose }) => {
-  const setAvatarPath = useSetAtom(avatarPathAtom);
-  const setBackgroundImagePath = useSetAtom(backgroundImagePathAtom);
-  const setTextToSpeechApiType = useSetAtom(textToSpeechApiTypeAtom);
-  
+export const SettingContainer: React.FC<SettingContainerProps> = ({
+  onClose,
+}) => {
+  const [avatarPath, setAvatarPath] = useAtom(avatarPathAtom);
+  const [backgroundImagePath, setBackgroundImagePath] = useAtom(
+    backgroundImagePathAtom
+  );
+  const [textToSpeechApiType, setTextToSpeechApiType] = useAtom(
+    textToSpeechApiTypeAtom
+  );
+
   const handleChangeAvatar = (e: ChangeEvent<HTMLSelectElement>) => {
     setAvatarPath(e.target.value);
   };
@@ -28,56 +43,30 @@ export const SettingContainer: React.FC<SettingContainerProps> = ({ onClose }) =
     setTextToSpeechApiType(e.target.value as TextToSpeechApiType);
   };
 
-
   return (
-    <div className="w-screen h-screen opacity-90 bg-black z-30 top-0 fixed text-white overflow-y-scroll">
-      <div className="flex justify-end  fixed top-0 right-0 mt-2 cursor-pointer">
-        <FaRegTimesCircle
-          className="text-white text-[34px] "
+    <div className="w-screen h-screen opacity-90 bg-black z-30 top-0 fixed overflow-y-scroll">
+      <div className="fixed top-0 right-0 p-3">
+        <button
+          className="text-white btn btn-circle btn-outline btn-xs"
           onClick={onClose}
-        />
+        >
+          <MdClose />
+        </button>
       </div>
       <div className="flex justify-center">
         <div className="flex flex-col items-center w-[300px] py-10">
-          {/*  言語選択UI ※現状多言語対応がないためコメントアウト */}
-          {/* <div className="mb-10 w-full">
-            <h2 className="mb-3 font-bold">言語を選ぶ</h2>
-            <div className="flex justify-between">
-              <button
-                className={`${
-                  selectedLanguage === "English"
-                    ? "bg-blue-400"
-                    : "bg-stone-400"
-                }  w-[120px] px-5 py-1 mr-5 text-center rounded-md`}
-                onClick={() => setSelectedLanguage("English")}
-              >
-                English
-              </button>
-              <button
-                className={`${
-                  selectedLanguage === "中文" ? "bg-blue-400" : "bg-stone-400"
-                }  w-[120px] px-5 py-1 text-center rounded-md`}
-                onClick={() => setSelectedLanguage("中文")}
-              >
-                中文
-              </button>
-            </div>
-          </div> */}
           {/* ユーザー情報表示・更新UI */}
-          <div className="w-full">
+          <div className="w-full pb-3 border-b-2">
             <UserInfo />
           </div>
           {/* アバターの変更UI */}
           <div className="w-full">
-            <h2 className="font-bold mb-5">アバターを選ぶ</h2>
+            <HeaderLabel>アバターを選ぶ</HeaderLabel>
             <select
-              className="rounded-md p-2 mb-5"
-              placeholder="選択する"
+              className="select select-bordered w-full max-w-xs"
               onChange={(e) => handleChangeAvatar(e)}
+              value={avatarPath}
             >
-              <option value="" disabled selected>
-                選択してください
-              </option>
               {avatars.map((avatar, index) => {
                 return (
                   <option key={index} value={avatar.path}>
@@ -89,15 +78,12 @@ export const SettingContainer: React.FC<SettingContainerProps> = ({ onClose }) =
           </div>
           {/* 背景の変更UI */}
           <div className="w-full">
-            <h2 className="font-bold mb-5">背景を選ぶ</h2>
+            <HeaderLabel>背景を選ぶ</HeaderLabel>
             <select
-              className="rounded-md p-2 mb-5"
-              placeholder="選択する"
+              className="select select-bordered w-full max-w-xs"
               onChange={(e) => handleChangeBackgroundImage(e)}
+              value={backgroundImagePath}
             >
-              <option value="" disabled selected>
-                選択してください
-              </option>
               {backgroundImages.map((image, index) => {
                 return (
                   <option key={index} value={image.path}>
@@ -109,15 +95,12 @@ export const SettingContainer: React.FC<SettingContainerProps> = ({ onClose }) =
           </div>
           {/* 背景の変更UI */}
           <div className="w-full">
-            <h2 className="font-bold mb-5">合成音声の種類を選ぶ</h2>
+            <HeaderLabel>合成音声の種類を選ぶ</HeaderLabel>
             <select
-              className="rounded-md p-2 mb-5"
-              placeholder="選択する"
+              className="select select-bordered w-full max-w-xs"
               onChange={(e) => handleChangeVoiceApi(e)}
+              value={textToSpeechApiType}
             >
-              <option value="" disabled selected>
-                選択してください
-              </option>
               {textToSpeechApiTypeList.map((textToSpeechApi, index) => {
                 return (
                   <option key={index} value={textToSpeechApi.value}>
@@ -128,7 +111,7 @@ export const SettingContainer: React.FC<SettingContainerProps> = ({ onClose }) =
             </select>
           </div>
           {/* サインアウトボタン */}
-          <div className="w-full">
+          <div className="w-full my-10">
             <button
               className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
               onClick={() => signOut()}
--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -3,6 +3,7 @@ import { Prefecture, UserGenderType } from "@/utils/types";
 import { prefectures } from "@/utils/constants";
 import { useUserInfo } from "@/hooks/useUserInfo";
 import { useCallback, useState } from "react";
+import { HeaderLabel } from "./HeaderLabel";
 
 const UserInfo = () => {
   const { editUserInfo, deleteUserInfo, userInfo, isEditMode, setIsEditMode } =
@@ -31,116 +32,135 @@ const UserInfo = () => {
   }, [deleteUserInfo]);
 
   return (
-    <>
-      <div className="flex justify-between">
-        <h2 className="mb-3 font-bold">ユーザー情報</h2>
-        {isEditMode ? (
-          <div className="flex items-center mb-5">
-            <button className="btn btn-error btn-xs" onClick={handleUserDelete}>
-              ユーザーの削除
+    <div className="w-full">
+      {isEditMode ? (
+        <form onSubmit={handleSubmit}>
+          <div className="flex justify-between items-center mb-5">
+            <button className="btn btn-neutral btn-sm" onClick={() => setIsEditMode(false)}>
+              キャンセル
+            </button>
+            <button type="submit" className="btn btn-primary btn-sm">
+              更新する
             </button>
           </div>
-        ) : (
-          <div
-            className="bg-stone-300 h-7 w-20 text-black text-center rounded-md cursor-pointer"
-            onClick={() => setIsEditMode(true)}
-          >
-            編集する
-          </div>
-        )}
-      </div>
-      {isEditMode ? (
-        <div className="mb-10">
-          <form onSubmit={handleSubmit}>
-            <div className="flex items-center mb-5">
-              <FaRegUserCircle
-                className={`text-[50px] -ml-1 mr-5 text-white self-center`}
-              />
-              <div>
-                <label htmlFor="">名前：</label>
-                <input
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  className="rounded-md p-2 w-full"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col mb-5">
-              <label>都道府県：</label>
-              <select
-                className="rounded-md p-2 mb-5"
-                value={prefecture}
-                onChange={(e) => setPrefecture(e.target.value as Prefecture)}
-              >
-                {prefectures.map((prefecture, index) => {
-                  return (
-                    <option key={index} value={prefecture}>
-                      {prefecture}
-                    </option>
-                  );
-                })}
-              </select>
-
-              <span>生年月日：</span>
-              <input
-                type="text"
-                className="rounded-md p-2 mb-5"
-                value={birthdate}
-                onChange={(e) => setBirthdate(e.target.value)}
-              />
-
-              <label htmlFor="">性別：</label>
-              <select
-                name=""
-                id=""
-                value={gender}
-                className="rounded-md p-2"
-                onChange={(e) => setGender(e.target.value as UserGenderType)}
-              >
-                <option value="男性">男性</option>
-                <option value="女性">女性</option>
-                <option value="選択しない">選択しない</option>
-              </select>
-            </div>
-            <div className="flex justify-between">
-              <div className="btn" onClick={() => setIsEditMode(false)}>
-                キャンセル
-              </div>
-              <button type="submit" className="btn btn-primary">
-                更新する
-              </button>
-            </div>
-          </form>
-        </div>
-      ) : (
-        <div className="w-full">
           <div className="flex flex-col mb-5">
-            <div className="flex mb-5 items-center">
-              <FaRegUserCircle
-                className={`text-[50px] -ml-1 mr-5 text-white self-start `}
-              />
-              <p className="text-2xl">{userInfo?.name}</p>
-            </div>
+            <label htmlFor="name">
+              <HeaderLabel>名前：</HeaderLabel>
+            </label>
+            <input
+              id="name"
+              type="text"
+              className="input"
+              pattern="[A-Za-z0-9]+"
+              title="英数字のみ入力してください"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
 
-            <div className="mb-5">
-              都道府県：
-              <p>{userInfo?.prefecture}</p>
-            </div>
-            <div className="mb-5">
-              誕生日：
-              <p>{userInfo?.birthdate}</p>
-            </div>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="prefecture">
+              <HeaderLabel>都道府県：</HeaderLabel>
+            </label>
+            <select
+              id="prefecture"
+              className="select select-bordered w-full max-w-xs"
+              value={prefecture}
+              onChange={(e) => setPrefecture(e.target.value as Prefecture)}
+            >
+              {prefectures.map((prefecture, index) => {
+                return (
+                  <option key={index} value={prefecture}>
+                    {prefecture}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
 
-            <div className="mb-5">
-              性別：
-              <p>{userInfo?.gender}</p>
-            </div>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="birthdate">
+              <HeaderLabel>生年月日：</HeaderLabel>
+            </label>
+            <input
+              id="birthdate"
+              type="text"
+              className="input"
+              pattern="\d{4}/\d{2}/\d{2}"
+              title="yyyy/mm/ddの形式で入力してください"
+              value={birthdate}
+              onChange={(e) => setBirthdate(e.target.value)}
+            />
+          </div>
+
+          <div className="flex flex-col mb-5">
+            <label htmlFor="gender">
+              <HeaderLabel>性別：</HeaderLabel>
+            </label>
+            <select
+              id="gender"
+              className="select select-bordered w-full max-w-xs"
+              value={gender}
+              onChange={(e) => setGender(e.target.value as UserGenderType)}
+            >
+              <option value="男性">男性</option>
+              <option value="女性">女性</option>
+              <option value="選択しない">選択しない</option>
+            </select>
+          </div>
+
+          <div className="flex justify-between items-center my-10">
+            <button className="btn btn-error btn-xs w-full" onClick={handleUserDelete}>
+              ユーザー情報を削除する
+            </button>
+          </div>
+        </form>
+      ) : (
+        <div>
+          <div className="flex justify-between items-center mb-5">
+            <p className="font-bold text-white text-2xl">ユーザー情報</p>
+            <button
+              className="btn btn-secondary btn-sm"
+              onClick={() => setIsEditMode(true)}
+            >
+              編集する
+            </button>
+          </div>
+          <div className="flex items-center mb-5">
+            <label htmlFor="name">
+              <HeaderLabel>名前：</HeaderLabel>
+            </label>
+            <p id="name" className="text-white text-xl mt-5 mb-3">
+              {userInfo?.name}
+            </p>
+          </div>
+          <div className="flex items-center mb-5">
+            <label htmlFor="prefecture">
+              <HeaderLabel>都道府県：</HeaderLabel>
+            </label>
+            <p id="prefecture" className="text-white text-xl mt-5 mb-3">
+              {userInfo?.prefecture}
+            </p>
+          </div>
+          <div className="flex items-center mb-5">
+            <label htmlFor="birthdate">
+              <HeaderLabel>生年月日：</HeaderLabel>
+            </label>
+            <p id="birthdate" className="text-white text-xl mt-5 mb-3">
+              {userInfo?.birthdate}
+            </p>
+          </div>
+          <div className="flex items-center mb-5">
+            <label htmlFor="gender">
+              <HeaderLabel>性別：</HeaderLabel>
+            </label>
+            <p id="gender" className="text-white text-xl mt-5 mb-3">
+              {userInfo?.gender}
+            </p>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
