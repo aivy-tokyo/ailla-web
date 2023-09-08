33132c6cb61599a48fa98045e0274df3bc5a470c
toki77 <r.toki77@gmail.com>
2023-09-08 10:30:12 +0900

    誕生日の入力欄の修正、名前アルファベットのみに修正 コミット漏れ

--- hooks/useUserInfo.ts
+++ hooks/useUserInfo.ts
@@ -4,15 +4,20 @@ import { Prefecture, UserGenderType } from "@/utils/types";
 import axios from "axios";
 import { useAtom, useAtomValue } from "jotai";
 import { signOut } from "next-auth/react";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect } from "react";
 
 export const useUserInfo = () => {
   const userId = useAtomValue(userIdAtom);
-  const [isEditMode, setIsEditMode] = useState<boolean>(false);
   const [userInfo, setUserInfo] = useAtom(userInfoAtom);
 
+  // userInfoが変更されるたびにログを吐き出す
+  useEffect(() => {
+    console.log("userInfo", userInfo);
+  }
+  , [userInfo]);
+
   const editUserInfo = useCallback(
-    (
+    async (
       userName: string,
       userPrefecture: Prefecture,
       userBirthdate: string,
@@ -20,35 +25,23 @@ export const useUserInfo = () => {
     ) => {
       if (!userId) return;
 
-      axios
-        .post(
-          `/api/user`,
-          {
-            id: userId,
-            name: userName,
-            prefecture: userPrefecture,
-            birthdate: userBirthdate,
-            gender: userGender,
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        )
-        .then((response) => {
-          const userInfo: UserInfo = {
-            name: response.data.name.S,
-            prefecture: response.data.prefecture.S,
-            birthdate: response.data.birthdate.S,
-            gender: response.data.gender.S,
-          };
-          setIsEditMode(false);
-          setUserInfo(userInfo);
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+      console.log("editUserInfo", userName, userPrefecture, userBirthdate, userGender);
+
+      const response = await axios.post("/api/user", {
+        id: userId,
+        name: userName,
+        prefecture: userPrefecture,
+        birthdate: userBirthdate,
+        gender: userGender,
+      });
+
+      const userInfo: UserInfo = {
+        name: response.data.name.S,
+        prefecture: response.data.prefecture.S,
+        birthdate: response.data.birthdate.S,
+        gender: response.data.gender.S,
+      };
+      setUserInfo(userInfo);
     },
     [userId, setUserInfo]
   );
@@ -73,8 +66,6 @@ export const useUserInfo = () => {
     editUserInfo,
     deleteUserInfo,
     userInfo,
-    setUserInfo,
-    isEditMode,
-    setIsEditMode,
+    setUserInfo
   };
 };
