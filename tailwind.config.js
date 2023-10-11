596fa1c1d0ba861d8610bae86f4e339176e45f7e
gclassuae <gclass.dev@gmail.com>
2023-10-11 19:33:47 +0900

    box shadow layout is fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
+    extend: {      
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -14,20 +14,13 @@ module.exports = {
         'gradient-46.5':
           'linear-gradient(46.5deg, #F8B856 6.73%, #EF858C 26.64%, #BA79B1 46.55%, #9070AF 66.46%, #7B59A3 86.37%)',
       },
+      boxShadow: {
+        'manner-mode': '0px 4px 16px 0px rgba(0, 0, 0, 0.15)',
+      },      
       transitionProperty: {
         'height': 'height',
       }
     },
   },
-  plugins: [
-    require("daisyui"),
-    function ({ addUtilities }) {
-      const newUtilities = {
-        '.gradient-46.5deg': {
-          background: 'linear-gradient(46.5deg, #F8B856 6.73%, #EF858C 26.64%, #BA79B1 46.55%, #9070AF 66.46%, #7B59A3 86.37%)',
-        },
-      };
-      addUtilities(newUtilities, ['responsive', 'hover']);
-    },
-  ],
+  plugins: [require("daisyui")],
 }
