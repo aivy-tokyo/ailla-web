c0192517dcecef17d3befd788b9000d4cf5a3a92
gclassuae <gclass.dev@gmail.com>
2023-10-11 20:48:18 +0900

    remove unnecessary custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,6 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'gradient-sphere':
-          'linear-gradient(46.5deg, #F8B856 6.73%, #EF858C 26.64%, #BA79B1 46.55%, #9070AF 66.46%, #7B59A3 86.37%)',
       },
       boxShadow: {
         'manner-mode': '0px 4px 16px 0px rgba(0, 0, 0, 0.15)',
