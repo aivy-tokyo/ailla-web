c4c10ac7c9264f21b3d2ecf24990d77dca264065
gclassuae <gclass.dev@gmail.com>
2023-10-23 19:29:31 +0900

    remove unnecessary module import

--- components/SettingContainer.tsx
+++ components/SettingContainer.tsx
@@ -1,18 +1,6 @@
-import React, { ChangeEvent } from "react";
-import { signOut } from "next-auth/react";
-import { useAtom, useSetAtom } from "jotai";
+import React from "react";
 import { MdClose } from "react-icons/md";
-import {
-  avatarPathAtom,
-  backgroundImagePathAtom,
-  currentAvatarAtom,
-} from "../utils/atoms";
-import {
-  avatars,
-  backgroundImages,
-} from "../utils/constants";
 import UserInfo from "./UserInfo";
-import { HeaderLabel } from "./HeaderLabel";
 
 interface SettingContainerProps {
   onClose: () => void;
@@ -21,24 +9,6 @@ interface SettingContainerProps {
 export const SettingContainer: React.FC<SettingContainerProps> = ({
   onClose,
 }) => {
-  const [avatarPath, setAvatarPath] = useAtom(avatarPathAtom);
-  const setCurrentAvatar = useSetAtom(currentAvatarAtom);
-  const [backgroundImagePath, setBackgroundImagePath] = useAtom(
-    backgroundImagePathAtom
-  );
-
-  const handleChangeAvatar = (e: ChangeEvent<HTMLSelectElement>) => {
-    const currentAvatar = avatars.find(avatar => avatar.path === e.target.value);
-    if(!currentAvatar)return;
-    setCurrentAvatar(currentAvatar);
-    setAvatarPath(e.target.value);
-  };
-
-  const handleChangeBackgroundImage = (e: ChangeEvent<HTMLSelectElement>) => {
-    setBackgroundImagePath(e.target.value);
-  };
-
-
   return (
     <div className="w-screen h-screen bg-[#E3DDE8] z-30 top-0 fixed overflow-y-scroll">
       <div className="fixed top-0 right-0 p-3">
