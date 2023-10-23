3c5ac8ec9b7fe7cb99d8bff2d57affa55af5c08d
gclassuae <gclass.dev@gmail.com>
2023-10-23 19:02:11 +0900

    format of the text in the upper part is fixed

--- components/HeaderLabel.tsx
+++ components/HeaderLabel.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export const HeaderLabel: React.FC<PropsWithChildren> = ({ children }) => (
-  <h2 className="text-base text-[#47556D] font-bold mt-5 mb-3">{children}</h2>
+  <h2 className="text-base text-[#47556D] font-bold">{children}</h2>
 );
--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -16,12 +16,12 @@ const UserInfoItem = ({
   id: string;
 }) => {
   return (
-    <div className="flex justify-between mb-5 w-3/5">
-      <label htmlFor={id} className="w-1/5">
+    <div className="flex justify-left w-3/5 my-4">
+      <label htmlFor={id} className="w-2/5">
         <HeaderLabel>{label}</HeaderLabel>
       </label>
       <div className="w-2/5">
-        <p id={id} className="text-[#47556D] text-left text-xl mt-5 mb-3">
+        <p id={id} className="text-[#47556D] text-left text-base">
           {value}
         </p>
       </div>
@@ -31,28 +31,22 @@ const UserInfoItem = ({
 
 const UserInfo = () => {
   const { editUserInfo, deleteUserInfo, userInfo } = useUserInfo();
-
   const [name, setName] = useState<string>(userInfo?.name as string);
   const [prefecture, setPrefecture] = useState<Prefecture>(
     userInfo?.prefecture as Prefecture,
   );
-
   const [birthdate, setBirthdate] = useState<string>(
     userInfo?.birthdate as string,
   );
-
   const [year, setYear] = useState<string>(
     userInfo?.birthdate.split("/")[0] as string,
   );
-
   const [month, setMonth] = useState<string>(
     userInfo?.birthdate.split("/")[1] as string,
   );
-
   const [day, setDay] = useState<string>(
     userInfo?.birthdate.split("/")[2] as string,
   );
-
   const [gender, setGender] = useState<UserGenderType>(
     userInfo?.gender as UserGenderType,
   );
