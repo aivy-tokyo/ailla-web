8fb5dbfc64e25c043eef33768a06b6638eb53c98
gclassuae <gclass.dev@gmail.com>
2023-10-24 14:59:16 +0900

    renual help dialog

--- components/HelpDialog.tsx
+++ components/HelpDialog.tsx
@@ -1,14 +1,23 @@
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
 export const HelpDialog = () => {
   return (
     <dialog id="modal_help" className="modal">
-      <div className="modal-box">
-        <h3 className="font-bold text-lg">使い方</h3>
+      <div className="modal-box bg-white/80 text-[#47556D]">
+        <div className="flex justify-between items-center">
+          <h2 className="font-bold text-2xl w-2/5">使い方</h2>
+          <form method="dialog" className="bg-transparent flex justify-end items-center h-full w-3/5">
+            {/* if there is a button in form, it will close the modal */}
+            <button>
+              <AiOutlineCloseCircle className="" size={30} />
+            </button>
+          </form>
+        </div>
         <p className="py-4">
           このアプリでは、Aillaと英語で会話をすることができます。
           <br />
           英会話の練習には3つのモードがあります。
-          <br />
-          <br />
+          <div className="w-full my-5 bg-[#CED4DE] border-b h-[0.1rem] border-[#CED4DE]" />
           <strong>1. フリートークモード</strong>
           <br />
           <strong>2. シチュエーションモード</strong>
@@ -16,6 +25,7 @@ export const HelpDialog = () => {
           <strong>3. リピートプラクティスモード</strong>
           <br />
           <br />
+          <div className="w-full my-2 bg-[#CED4DE] border-b h-[0.1rem] border-[#CED4DE]" />
           <strong>フリートークモード</strong>
           では、自由に会話をすることができます。
           <br />
@@ -28,13 +38,7 @@ export const HelpDialog = () => {
           <br />
           モードを選択すると、会話が始まります。
         </p>
-        <div className="modal-action">
-          <form method="dialog" className="flex justify-center w-full">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn">とじる</button>
-          </form>
-        </div>
       </div>
     </dialog>
-  )
-}
+  );
+};
