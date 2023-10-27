39a514c6bb14af468f1641a94c93380d7de17c2b
toki77 <r.toki77@gmail.com>
2023-10-27 17:19:19 +0900

    日本語対応

--- entities/ClientInfo.ts
+++ entities/ClientInfo.ts
@@ -1,12 +1,12 @@
 export interface ClientInfo {
   // 省略言語
-  language: "cn" | "en";
+  language: "cn" | "en" | "ja";
   // 正式言語
-  formalLanguage: "zh-CN" | "en-US";
+  formalLanguage: "zh-CN" | "en-US" | "ja-JP";
   // 学習言語
-  learningLanguage: "中国語" | "英語";
+  learningLanguage: "中国語" | "英語" | "日本語";
   // 話してほしい言語
-  speakLanguage: "北京語" | "英語";
+  speakLanguage: "北京語" | "英語" | "日本語";
   // シチュエーションリスト
   situationList: string[];
   // 導入時の挨拶
